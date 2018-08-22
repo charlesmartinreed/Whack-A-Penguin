315a9a2616e718383ae0c0fd0b45bbf48d0b91e4
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-08-22 13:57:05 -0500

    Milestone commit. Penguins now show and hide. Not yet hittable.

--- Project14/GameScene.swift
+++ Project14/GameScene.swift
@@ -7,22 +7,26 @@
 //
 
 import SpriteKit
-
-var gameScore: SKLabelNode!
-var score = 0 {
-    didSet {
-        gameScore.text = "Score: \(score)"
-    }
-}
-
-//this array will hold our slots
-var slots = [WhackSlot]()
-
-//this is where we'll store the penguin node
-var charNode: SKSpriteNode!
-
+import GameplayKit
 
 class GameScene: SKScene {
+   
+    //this array will hold our slots
+    var slots = [WhackSlot]()
+    
+    var gameScore: SKLabelNode!
+    var score = 0 {
+        didSet {
+            gameScore.text = "Score: \(score)"
+        }
+    }
+    
+    //this is where we'll store the penguin node
+    var charNode: SKSpriteNode!
+    
+    //we'll use this to vary how quickly penguins appear visible
+    var popupTime = 0.85
+    
     override func didMove(to view: SKView) {
         //adding our background for the game level
         let background = SKSpriteNode(imageNamed: "whackBackground")
@@ -46,12 +50,26 @@ class GameScene: SKScene {
         for i in 0..<5 { createSlot(at: CGPoint(x: 100 + (i * 170), y: 230))}
         for i in 0..<4 { createSlot(at: CGPoint(x: 180 + (i * 170), y: 140))}
         
+        //after a brief delay to allow the user to orient themselves, we start creating penguins
+        //asyncAfter means "1 second after our deadline, in this case "now"
+//        DispatchQueue.main.asyncAfter(deadline: .now()) {
+//            [unowned self] in
+//            self.doStuff()
+//        }
+        
+        //triggering our call createEnemy - keep in mind this function calls itself once this initial call is completed
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            [unowned self] in
+            self.createEnemy()
+        }
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         //stuff
     }
     
+    //MARK: - createSlot()
     func createSlot(at position: CGPoint) {
         //method accepts position, creates a WhackSlot object and calls the configure method before adding the slot to the scene AND the array
         
@@ -60,5 +78,29 @@ class GameScene: SKScene {
         addChild(slot)
         slots.append(slot)
     }
-   
+    
+    //MARK: - createEnemy()
+    func createEnemy() {
+        //decreasing the popupTime by a fraction each time this is run
+        popupTime *= 0.991
+        
+        slots = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: slots) as! [WhackSlot]
+        slots[0].show(hideTime: popupTime)
+        
+        //using a random int to decide which of the shuffled array items to use for placing our game sprites after our initial one is shown.
+        if RandomInt(min: 0, max: 12) > 4 { slots[1].show(hideTime: popupTime) }
+        if RandomInt(min: 0, max: 12) > 8 { slots[2].show(hideTime: popupTime) }
+        if RandomInt(min: 0, max: 12) > 10 { slots[3].show(hideTime: popupTime) }
+        if RandomInt(min: 0, max: 12) > 11 { slots[4].show(hideTime: popupTime) }
+        
+        let minDelay = popupTime / 2.0
+        let maxDelay = popupTime * 2
+        let delay = RandomDouble(min: minDelay, max: maxDelay)
+        
+        //this allows our createEnemy func to call itself after a random period of time from the present until the game is done
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+            [unowned self] in
+            self.createEnemy()
+        }
+    }
 }
--- Project14/WhackSlot.swift
+++ Project14/WhackSlot.swift
@@ -11,7 +11,12 @@ import SpriteKit
 
 //SKNode is the base class that subclasses like SKSpriteNode, SKEmitterNode, SKLabelNode, etc. extend. It simply sits at a position in our scene and holds child nodes.
 class WhackSlot: SKNode {
-    //if you don't create any inits or required inits and don't use any non-optional properties, Swift will use the parent class's init methods
+    //NOTE: No init? If you don't create any inits or required inits and don't use any non-optional properties, Swift will use the parent class's init methods.
+    
+    var charNode: SKSpriteNode!
+    //we'll use these properties to track when the penguin is visible and whether or not it was successfully hit when visible
+    var isVisible = false
+    var isHit = false
     
     func configure(at position: CGPoint) {
         self.position = position
@@ -37,6 +42,38 @@ class WhackSlot: SKNode {
         
         //and then we add the cropNode as a child of our WhackSlot
         addChild(cropNode)
+    }
+    
+    //this will handle our game logic - show the penguin if not already visible, allow the player to hit the penguin
+    func show(hideTime: Double) {
+        if isVisible { return }
+        
+        //this animation creates the effect of our penguin popping out of the hole
+        charNode.run(SKAction.moveBy(x: 0, y: 80, duration: 0.05))
+        
+        isVisible = true
+        isHit = false
+        
+        if RandomInt(min: 0, max: 2) == 0 {
+            charNode.texture = SKTexture(imageNamed: "penguinGood")
+            charNode.name = "charFriend"
+        } else {
+            charNode.texture = SKTexture(imageNamed: "penguinEvil")
+            charNode.name = "charEnemy"
+        }
+        
+        //triggering our hide function
+        DispatchQueue.main.asyncAfter(deadline: .now() + (hideTime * 3.5)) {
+            [unowned self] in
+            self.hide()
+        }
+    }
+    
+    func hide() {
+        if !isVisible { return }
+        
+        charNode.run(SKAction.moveBy(x: 0, y: -80, duration: 0.05))
+        isVisible = false
     }
 
 }
