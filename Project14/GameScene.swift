73410e598c2c24a2a3df215f5c5f2c0ab09d80cd
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-08-22 12:23:13 -0500

    Initial Commit

---
+++ Project14/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  Project14
+//
+//  Created by Charles Martin Reed on 8/22/18.
+//  Copyright © 2018 Charles Martin Reed. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        //stuff
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //stuff
+    }
+   
+}
